f61c0740d41a4ded4ed87a60fb3de74bfff9d03e
joanna-pham <jopham90@gmail.com>
2021-07-24 23:56:29 -0700

    create card component for portfolio

--- main.js
+++ main.js
@@ -216,3 +216,12 @@ document.body.appendChild(stats.domElement);
 //   requestAnimationFrame(update);
 // };
 // requestAnimationFrame(update);
+
+function myFunction() {
+  var x = document.getElementById("navigation");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
